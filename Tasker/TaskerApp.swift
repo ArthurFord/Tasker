f5e4491603340b0c5c5ffd05df915a8faa61403a
Artie Ford <adf.ford@gmail.com>
2021-10-26 06:11:10 -0400

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Arthur Ford on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
